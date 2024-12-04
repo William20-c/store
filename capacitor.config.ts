fbb3b18205d67a35bd262a4f51d4857d4f93cfa6
William Castaño <williamcastano05@gmail.com>
2024-12-04 17:40:19 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'store',
+  webDir: 'dist'
+};
+
+export default config;
